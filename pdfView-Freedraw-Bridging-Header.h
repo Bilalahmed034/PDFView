36d3bb1c99f95315cc1a957c05b64a247d8bd167
Bilal Ahmed <bilalahmed034@gmail.com>
2024-03-21 11:31:54 +0500

    initial commit

---
+++ pdfView-Freedraw-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  pdfView-Freedraw-Bridging-Header.h
+//  pdfView Freedraw
+//
+//  Created by Bilal Ahmed on 02/01/2024.
+//
+
+#import <PerformanceBezier/PerformanceBezier.h>
+#import <ClippingBezier/ClippingBezier.h>
